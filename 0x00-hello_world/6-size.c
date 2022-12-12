d8c844bcbccf3a15a5dd8fa6930c046a0168b250
nashlee <lilynashon@gmail.com>
2022-12-12 00:32:45 -0800

    size of data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
-#include <stdio.h>                                                                                                                      
-
-/**                                                                                                                                     
+#include <stdio.h>
+/**
+ * main - print size of datatypes in C
  *
- * main - prints sizes of datatypes in C 
- *
- * Return: Always 0 (Success) 
- */                                                                                                                                     int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-        printf("Size of a char: %zu byte(s)\n", sizeof(char));                                                                                  printf("Size of a int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a longint: %zu byte(s)\n", sizeof(long int));                                          	     
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));                                           
-      	printf("Size of a float: %zu byte(s)\n", sizeof(float));                                                                      
-	return (0);                                                                                                                    
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a longint: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
