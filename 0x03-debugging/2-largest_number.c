995f35c427aea969a7e8fcdb936633c1be620d2b
nashlee <lilynashon@gmail.com>
2022-12-16 01:38:02 -0800

    debugging task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -18,12 +18,10 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	/**
-	*else if	(c > b)
-	*{
-	*	largest = c;
+	else if	(c > b)
+	{
+		largest = c;
 	}
-	*/
 	else
 	{
 		largest = c;
