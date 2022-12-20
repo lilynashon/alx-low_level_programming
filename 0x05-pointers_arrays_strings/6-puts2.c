0e8b0be9a37eaeb3da729ebd6777e29dcb81a746
nashlee <lilynashon@gmail.com>
2022-12-19 18:33:18 -0800

    task 6 print every character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * puts2 -  function that prints every other character of a string
+ * @str: ibput
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+
+	int t = 0;
+
+	char *y = str;
+
+	int 0;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+
+	t = longi - 1;
+
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
+}
